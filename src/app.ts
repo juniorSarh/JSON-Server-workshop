916486be5437b1b20bba0215f73cbc197b29a137
juniorSarh <sabelohgumede@gmail.com>
2025-08-29 11:23:11 +0200

    pushing on a new branch called new-feature-greeting

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,8 @@ async function fetchData() {
   
     const response = await fetch("http://localhost:3000/items");
     const sec_response = await response.json()
-    console.log(sec_response); 
+    characters =sec_response
+    console.log(characters[1]); 
 }
 
 fetchData();
