a380fdbf41e9bec6a5cdd95858997fc9f2495fea
juniorSarh <sabelohgumede@gmail.com>
2025-08-29 11:10:37 +0200

    working on dev banch

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+let characters;
 function fetchData() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch("http://localhost:3000/items");
         const sec_response = yield response.json();
-        console.log(sec_response);
+        //console.log(sec_response); 
+        characters = sec_response;
+        console.log(characters[5]);
+        const listElement = document.getElementById('itemsList');
+        if (listElement) {
+            listElement.innerHTML = ''; // Clear existing items
+            characters.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = `${item.id}: ${item.name}`;
+                listElement.appendChild(li);
+            });
+        }
     });
 }
-fetchData();
+window.onload = () => {
+    fetchData();
+};
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 //import axios from 'axios';
 
+let characters:any[]
+
 async function fetchData() {
   
     const response = await fetch("http://localhost:3000/items");
     const sec_response = await response.json()
-    console.log(sec_response); 
-}
+    //console.log(sec_response); 
+    characters= sec_response
+    console.log(characters[5]);
 
-fetchData();
+    const listElement = document.getElementById('itemsList');
+
+    if (listElement) {
+      listElement.innerHTML = ''; // Clear existing items
+
+      characters.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.id}: ${item.name}`;
+        listElement.appendChild(li);
+      });
+    }
+ }
+window.onload=()=>{
+    fetchData();
+};
+
+
